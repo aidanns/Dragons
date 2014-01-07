50b972fa25ffb49df7a4b297e635203a8cd33562
Aidan Nagorcka-Smith <aidanns@gmail.com>
2014-01-07 20:07:13 +1100

    Added a basic test for the GameState.

---
+++ core/src/test/java/au/edu/unimelb/cis/dragons/core/GameStateTest.java
@@ -0,0 +1,36 @@
+package au.edu.unimelb.cis.dragons.core;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import au.edu.unimelb.cis.dragons.core.GameState.Key;
+import react.Value;
+
+public class GameStateTest {
+
+	private GameState _gameState;
+	
+	@Before
+	public void setUp() throws Exception {
+		_gameState = new GameState();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		_gameState = null;
+	}
+
+	@Test
+	public void storageWorks() {
+		Value<String> myValue = Value.create("value");
+		Key key = GameState.Key.CurrentGold;
+		
+		_gameState.addValueForKey(myValue, key);
+		
+		assertEquals(myValue, _gameState.valueForKey(key));
+	}
+
+}
