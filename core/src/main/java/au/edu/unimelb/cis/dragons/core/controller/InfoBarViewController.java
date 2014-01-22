996d4c4a0b1bc9c9d53c2f99eabcc56f4b922774
Aidan Nagorcka-Smith <aidanns@gmail.com>
2014-01-22 12:21:32 +1100

    Refactored the logic for composing the valueviews away from UI in to models.

--- core/src/main/java/au/edu/unimelb/cis/dragons/core/controller/InfoBarViewController.java
+++ core/src/main/java/au/edu/unimelb/cis/dragons/core/controller/InfoBarViewController.java
@@ -1,17 +1,9 @@
 package au.edu.unimelb.cis.dragons.core.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import au.edu.unimelb.cis.dragons.core.MultipleSourceValueViewBuilder;
 import au.edu.unimelb.cis.dragons.core.model.Farm;
-import au.edu.unimelb.cis.dragons.core.model.Dragon.DragonState;
-import au.edu.unimelb.cis.dragons.core.model.Farm.PenState;
 import au.edu.unimelb.cis.dragons.core.model.User;
 import au.edu.unimelb.cis.dragons.core.model.Wallet;
 import react.Function;
-import react.UnitSlot;
-import react.ValueView;
 import tripleplay.ui.Group;
 import tripleplay.ui.Shim;
 import tripleplay.ui.SizableGroup;
@@ -52,88 +44,45 @@ public class InfoBarViewController extends ContainerViewController {
 
 	@Override
 	protected Group createInterface() {
+		
+		Function<Integer, String> integerToStringMapper = new Function<Integer, String>() {
+			@Override
+			public String apply(Integer input) {
+				return input.toString();
+			}
+		};
+		
 		// Group to hold the top bar.
 		SizableGroup group = new SizableGroup(new AxisLayout.Horizontal());
 		group.setStyles(Styles.make(Style.HALIGN.left));
 		group.add(new Shim(5, 5));
 
-		TopBarEntryViewController userNameView = new TopBarEntryViewController("Username");
+		TopBarEntryViewController userNameView = new TopBarEntryViewController("User");
 		_user.name().connectNotify(userNameView.valueLabel().text.slot());
 		group.add(userNameView.view());
 
-		TopBarEntryViewController goldView = new TopBarEntryViewController("Gold");
-		_wallet.gold().map(new Function<Integer, String>() {
-			@Override
-			public String apply(Integer input) {
-				return input.toString();
-			}
-		}).connectNotify(goldView.valueLabel().text.slot());
+		TopBarEntryViewController goldView = new TopBarEntryViewController("$");
+		_wallet.gold().map(integerToStringMapper).connectNotify(goldView.valueLabel().text.slot());
 		group.add(goldView.view());
 
-		// Calculate dragons owned by summing all pens that are full.
-		List<ValueView<PenState>> pens = new ArrayList<ValueView<PenState>>(8);
-		for (int col = 0; col < 4; ++col) {
-			for (int row = 0; row < 2; ++row) {
-				pens.add(_farm.stateForPen(row, col));
-			}
-		}
 		TopBarEntryViewController dragonsOwnedView = new TopBarEntryViewController("Own");
-		new MultipleSourceValueViewBuilder<PenState, String>(new Function<List<ValueView<PenState>>, String>() {
-			@Override
-			public String apply(List<ValueView<PenState>> input) {
-				Integer i = 0;
-				for (ValueView<PenState> state : input) {
-					if (state.get() == PenState.Full) {
-						++i;
-					}
-				}
-				return i.toString();
-			}
-		}, pens).valueView().connectNotify(dragonsOwnedView.valueLabel().text.slot());
+		_farm.numberOfDragons().map(integerToStringMapper).connectNotify(dragonsOwnedView.valueLabel().text.slot());
 		group.add(dragonsOwnedView.view());
 
 		// Calculate dragons available by summing all dragons that have the state available.
 		TopBarEntryViewController dragonsAvailableView = new TopBarEntryViewController("Avail");
-		final MultipleSourceValueViewBuilder<DragonState, String> dragonStateBuilder = new MultipleSourceValueViewBuilder<DragonState, String>(new Function<List<ValueView<DragonState>>, String>() {
-			@Override
-			public String apply(List<ValueView<DragonState>> input) {
-				Integer i = 0;
-				for (ValueView<DragonState> dragonState : input) {
-					if (dragonState.get() == DragonState.Available) {
-						++i;
-					}
-				}
-				return i.toString();
-			}
-		});
-		dragonStateBuilder.valueView().connectNotify(dragonsAvailableView.valueLabel().text.slot());
-		// Need two levels of mapping because the number of available dragons needs
-		// to be updated when either a dragon's state changes, or a dragon changes.
-		// When the dragon changes, we also need to update with the extra dragon state
-		// that we now have access to.
-		final UnitSlot rebuildDragonStateBuilderSources = new UnitSlot() {
-			@Override
-			public void onEmit() {
-				dragonStateBuilder.clearSources();
-				for (int row = 0; row < 2; ++row) {
-					for (int col = 0; col < 4; ++col) {
-						if (_farm.dragonForPen(row, col).get() != null) {
-							dragonStateBuilder.addSource(_farm.dragonForPen(row, col).get().state());
-						}
-					}
-				}
-			}
-		};
-		for (int row = 0; row < 2; ++row) {
-			for (int col = 0; col < 4; ++col) {
-				_farm.dragonForPen(row, col).connect(rebuildDragonStateBuilderSources);
-			}
-		}
-		// Push the first update.
-		rebuildDragonStateBuilderSources.onEmit();
-
-		//.valueView().connectNotify(dragonsAvailableView.valueLabel().text.slot());
+		_farm.numberOfDragonsAvailable().map(integerToStringMapper).connectNotify(dragonsAvailableView.valueLabel().text.slot());
 		group.add(dragonsAvailableView.view());
+		
+		// Calculate dragons racing by summing all dragons that have the state racing.
+		TopBarEntryViewController dragonsRacingView = new TopBarEntryViewController("Race");
+		_farm.numberOfDragonsRacing().map(integerToStringMapper).connectNotify(dragonsRacingView.valueLabel().text.slot());
+		group.add(dragonsRacingView.view());
+		
+		// Calculate dragons breeding by summing all dragons that have the state racing.
+		TopBarEntryViewController dragonsBreedingView = new TopBarEntryViewController("Breed");
+		_farm.numberOfDragonsBreeding().map(integerToStringMapper).connectNotify(dragonsBreedingView.valueLabel().text.slot());
+		group.add(dragonsBreedingView.view());
 
 		return group;
 	}
--- core/src/main/java/au/edu/unimelb/cis/dragons/core/model/Farm.java
+++ core/src/main/java/au/edu/unimelb/cis/dragons/core/model/Farm.java
@@ -1,11 +1,15 @@
 package au.edu.unimelb.cis.dragons.core.model;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import react.Function;
+import react.UnitSlot;
 import react.ValueView;
 import au.edu.unimelb.cis.dragons.core.GameState;
+import au.edu.unimelb.cis.dragons.core.MultipleSourceValueViewBuilder;
+import au.edu.unimelb.cis.dragons.core.model.Dragon.DragonState;
 
 /**
  * Representation of the farm. Wraps the GameState to give simplified access to
@@ -13,7 +17,7 @@ import au.edu.unimelb.cis.dragons.core.GameState;
  * @author Aidan Nagorcka-Smith (aidanns@gmail.com)
  */
 public class Farm {
-	
+
 	/**
 	 * Represents the different possible states for a pen within the farm.
 	 */
@@ -21,39 +25,39 @@ public class Farm {
 
 		/** The pen has not yet been unlocked. */
 		Locked("locked"),
-		
+
 		/** The pen is unlocked, but no dragon is in it. */
 		Empty("empty"),
-		
+
 		/** The pen is unlocked and has a dragon in it. */
 		Full("full");
-		
+
 		// Map from the state name back to the state, to make it easy to pass
 		// back the correct PenState object after retrieving it's name from the
 		// GameState.
 		private static Map<String, PenState> stateNameToPenState = new HashMap<String, PenState>();
-		
+
 		// Create the Map from the state names back to the state objects.
 		static {
 			for (PenState penState : PenState.values()) {
 				stateNameToPenState.put(penState._stateName, penState);
 			}
 		}
-		
-		/** 
+
+		/**
 		 * The name for this state, which will be stored as a value in the
 		 * GameState.
 		 */
 		private String _stateName;
-		
+
 		PenState(String stateName) {
 			_stateName = stateName;
 		}
-		
+
 		public String toString() {
 			return _stateName;
 		}
-		
+
 		/**
 		 * Get the PenState for a given state name, which is returned from the
 		 * GameState.
@@ -64,10 +68,10 @@ public class Farm {
 			return stateNameToPenState.get(stateName);
 		}
 	}
-	
+
 	// The underlying game state.
 	private GameState _gameState;
-	
+
 	/**
 	 * Create a new FarmModel, wrapping the supplied GameState.
 	 * @param gameState The GameState that this FarmModel will give access to.
@@ -75,7 +79,7 @@ public class Farm {
 	public Farm(GameState gameState) {
 		_gameState = gameState;
 	}
-	
+
 	/**
 	 * Get the PenState for the pen at a certain index.
 	 * @param row The row index.
@@ -90,7 +94,7 @@ public class Farm {
 			}
 		});
 	}
-	
+
 	/**
 	 * Set a given pen to be close, removing any dragon that was previous in it.
 	 * @param row The row of the pen.
@@ -100,7 +104,7 @@ public class Farm {
 		_gameState.valueForKey(GameState.Key.penStateKeyAtIndex(row, column)).update(PenState.Locked.toString());
 		_gameState.valueForKey(GameState.Key.penDragonIdKeyAtIndex(row, column)).update("");
 	}
-	
+
 	/**
 	 * Set a given pen to be open, removing any dragon that was previously in it.
 	 * @param row The row of the pen.
@@ -110,7 +114,7 @@ public class Farm {
 		_gameState.valueForKey(GameState.Key.penStateKeyAtIndex(row, column)).update(PenState.Empty.toString());
 		_gameState.valueForKey(GameState.Key.penDragonIdKeyAtIndex(row, column)).update("");
 	}
-	
+
 	/**
 	 * Get the Dragon for the pen at a certain index.
 	 * @param row The row index.
@@ -126,7 +130,7 @@ public class Farm {
 			}
 		});
 	}
-	
+
 	/**
 	 * Set the Dragon that inhabits a certain pen, opening it if needed.
 	 * @param dragon The dragon that should be in the pen.
@@ -136,5 +140,142 @@ public class Farm {
 	public void setDragonForPen(Dragon dragon, Integer row, Integer column) {
 		_gameState.valueForKey(GameState.Key.penStateKeyAtIndex(row, column)).update(PenState.Full.toString());
 		_gameState.valueForKey(GameState.Key.penDragonIdKeyAtIndex(row, column)).update(dragon.id().toString());
+	}
+
+	public ValueView<Integer> numberOfDragons() {
+		 MultipleSourceValueViewBuilder<PenState, Integer> viewBuilder = new MultipleSourceValueViewBuilder<PenState, Integer>(new Function<List<ValueView<PenState>>, Integer>() {
+			@Override
+			public Integer apply(List<ValueView<PenState>> input) {
+				Integer i = 0;
+				for (ValueView<PenState> state : input) {
+					if (state.get() == PenState.Full) {
+						++i;
+					}
+				}
+				return i;
+			}
+		});
+		for (int col = 0; col < 4; ++col) {
+			for (int row = 0; row < 2; ++row) {
+				viewBuilder.addSource(stateForPen(row, col));
+			}
+		}
+		return viewBuilder.valueView();
+	}
+
+	private MultipleSourceValueViewBuilder<DragonState, Integer> _dragonsAvailableBuilder = null;
+	private MultipleSourceValueViewBuilder<DragonState, Integer> _dragonsRacingBuilder = null;
+	private MultipleSourceValueViewBuilder<DragonState, Integer> _dragonsBreedingBuilder = null;
+	private MultipleSourceValueViewBuilder<DragonState, Integer> _dragonsTrainingBuilder = null;
+
+	public ValueView<Integer> numberOfDragonsAvailable() {
+		if (_dragonsAvailableBuilder == null) {
+			createDragonStateViews();
+		}
+		return _dragonsAvailableBuilder.valueView();
+	}
+	public ValueView<Integer> numberOfDragonsRacing() {
+		if (_dragonsRacingBuilder == null) {
+			createDragonStateViews();
+		}
+		return _dragonsRacingBuilder.valueView();
+	}
+	public ValueView<Integer> numberOfDragonsBreeding() {
+		if (_dragonsBreedingBuilder == null) {
+			createDragonStateViews();
+		}
+		return _dragonsBreedingBuilder.valueView();
+	}
+	public ValueView<Integer> numberOfDragonsTraining() {
+		if (_dragonsTrainingBuilder == null) {
+			createDragonStateViews();
+		}
+		return _dragonsTrainingBuilder.valueView();
+	}
+
+	private void createDragonStateViews() {
+		// Calculate dragons available by summing all dragons that have the state available.
+		_dragonsAvailableBuilder = new MultipleSourceValueViewBuilder<DragonState, Integer>(new Function<List<ValueView<DragonState>>, Integer>() {
+			@Override
+			public Integer apply(List<ValueView<DragonState>> input) {
+				Integer i = 0;
+				for (ValueView<DragonState> dragonState : input) {
+					if (dragonState.get() == DragonState.Available) {
+						++i;
+					}
+				}
+				return i;
+			}
+		});
+
+		// Calculate dragons racing by summing all dragons that have the state racing.
+		_dragonsRacingBuilder = new MultipleSourceValueViewBuilder<DragonState, Integer>(new Function<List<ValueView<DragonState>>, Integer>() {
+			@Override
+			public Integer apply(List<ValueView<DragonState>> input) {
+				Integer i = 0;
+				for (ValueView<DragonState> dragonState : input) {
+					if (dragonState.get() == DragonState.Racing) {
+						++i;
+					}
+				}
+				return i;
+			}
+		});
+
+		// Calculate dragons breeding by summing all dragons that have the state racing.
+		_dragonsBreedingBuilder = new MultipleSourceValueViewBuilder<DragonState, Integer>(new Function<List<ValueView<DragonState>>, Integer>() {
+			@Override
+			public Integer apply(List<ValueView<DragonState>> input) {
+				Integer i = 0;
+				for (ValueView<DragonState> dragonState : input) {
+					if (dragonState.get() == DragonState.Breeding) {
+						++i;
+					}
+				}
+				return i;
+			}
+		});
+
+		_dragonsTrainingBuilder = new MultipleSourceValueViewBuilder<DragonState, Integer>(new Function<List<ValueView<DragonState>>, Integer>() {
+			@Override
+			public Integer apply(List<ValueView<DragonState>> input) {
+				Integer i = 0;
+				for (ValueView<DragonState> dragonState : input) {
+					if (dragonState.get() == DragonState.Training) {
+						++i;
+					}
+				}
+				return i;
+			}
+		});
+
+		// Need two levels of mapping because the number of available dragons needs
+		// to be updated when either a dragon's state changes, or a dragon changes.
+		// When the dragon changes, we also need to update with the extra dragon state
+		// that we now have access to.
+		final UnitSlot rebuildDragonStateBuilderSources = new UnitSlot() {
+			@Override
+			public void onEmit() {
+				_dragonsAvailableBuilder.clearSources();
+				_dragonsRacingBuilder.clearSources();
+				_dragonsBreedingBuilder.clearSources();
+				_dragonsTrainingBuilder.clearSources();
+				for (int row = 0; row < 2; ++row) {
+					for (int col = 0; col < 4; ++col) {
+						if (dragonForPen(row, col).get() != null) {
+							_dragonsAvailableBuilder.addSource(dragonForPen(row, col).get().state());
+							_dragonsRacingBuilder.addSource(dragonForPen(row, col).get().state());
+							_dragonsBreedingBuilder.addSource(dragonForPen(row, col).get().state());
+							_dragonsTrainingBuilder.addSource(dragonForPen(row, col).get().state());
+						}
+					}
+				}
+			}
+		};
+		for (int row = 0; row < 2; ++row) {
+			for (int col = 0; col < 4; ++col) {
+				dragonForPen(row, col).connect(rebuildDragonStateBuilderSources);
+			}
+		}
 	}
 }
